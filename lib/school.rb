6e36de20a2c40acd0a6cd31ff193dfb98ae1673a
Owen Cruse <ocruse7@gmail.com>
2020-01-07 16:31:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+  attr_reader :name, :roster, :add_student
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @student_name = name
+    @student_grade = grade
+    @roster[@student_grade] ||= []; @roster[@student_grade] << @student_name
+  end
+  
+  def grade(grade)
+    @key = grade
+    @roster[@key].collect do |students|
+     students
+    end
+  end
+  
+  def sort
+    @roster.each do |key,value|
+      @roster[key] = value.sort
+    end
+    @roster
+  end
+  
+end
